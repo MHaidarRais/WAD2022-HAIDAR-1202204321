e187fcff4680caf9f2be2723b3068b6b93a73e2d
MHaidarRais <zwingfame@gmail.com>
2022-11-20 20:25:26 +0700

    added sql

---
+++ MODUL3 HAIDAR/showroom_haidar_table.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3308
+-- Generation Time: Nov 20, 2022 at 02:13 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `showroom_haidar_table`
+--
+
+CREATE TABLE `showroom_haidar_table` (
+  `id_mobil` int(255) NOT NULL,
+  `nama_mobil` varchar(255) NOT NULL,
+  `pemilik_mobil` varchar(255) NOT NULL,
+  `merk_mobil` varchar(255) NOT NULL,
+  `tanggal_beli` date NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto_mobil` varchar(255) NOT NULL,
+  `status_pembayaran` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `showroom_haidar_table`
+--
+
+INSERT INTO `showroom_haidar_table` (`id_mobil`, `nama_mobil`, `pemilik_mobil`, `merk_mobil`, `tanggal_beli`, `deskripsi`, `foto_mobil`, `status_pembayaran`) VALUES
+(1956994134, 'asd', 'MUHAMMAD HAIDAR RAIS - 1202204321', 'asd', '2001-01-01', 'asdasd', 'DALL·E 2022-11-15 20.42.19 - arcteryx and rick owens  collaboration shoes.png', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
